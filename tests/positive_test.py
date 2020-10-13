24de0db0d94f411d468a735d806a36464c5b1c9f
Anna Tkachuk <67064207+tkachuk703@users.noreply.github.com>
2020-10-13 15:47:44 +0700

    Update positive_test.py

--- tests/positive_test.py
+++ tests/positive_test.py
@@ -1,10 +1,17 @@
-import pytest
-from app.Calculator import Calculator
-
+from app.calculator import Calculator
 
 class TestCalc:
     def setup(self):
-        self.сalc = Calculator
+        self.calc = Calculator
 
     def test_multiply_calculate_correctly(self):
-        assert self.сalc.multiply(self, 3, 3) == 9
+        assert self.calc.multiply(self, 2, 2) == 4
+
+    def test_division_calculate_correctly(self):
+        assert self.calc.division(self, 6, 2) == 3
+
+    def test_adding_calculate_correctly(self):
+        assert self.calc.adding(self, 3, 4) == 7
+
+    def test_substraction_calculate_correctly(self):
+        assert self.calc.subtraction(self, 10, 5) == 5
